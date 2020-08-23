2344d0c3692ef0e472d121f3a8f881341b281211
Joren Vrancken <jorenvrancken@gmail.com>
2020-08-23 19:59:13 +0200

    Update display outputs in dock script

--- roles/xorg/files/dock.sh
+++ roles/xorg/files/dock.sh
@@ -2,7 +2,7 @@
 
 pkill -f "polybar --reload --quiet default"
 
-xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --output DVI-I-1-1 --mode 1920x1080 --pos 1920x0 --output DVI-I-2-2 --mode 1920x1080 --pos 3840x0
+xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --output DVI-I-2-2 --mode 1920x1080 --pos 1920x0 --output DVI-I-1-1 --mode 1920x1080 --pos 3840x0
 
 MONITOR="DVI-I-1-1" polybar --reload --quiet default &
 MONITOR="DVI-I-2-2" polybar --reload --quiet default &
