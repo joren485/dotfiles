48cda9cb94fedcefea7ba0e3e6ae26eec4a3e4cd
Joren Vrancken <jorenvrancken@gmail.com>
2021-03-30 19:53:51 +0200

    Use altgr-intl keyboard layout

--- roles/xorg/files/dock.sh
+++ roles/xorg/files/dock.sh
@@ -13,6 +13,6 @@ xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --output DVI-I-2-2 --
 MONITOR="DVI-I-1-1" polybar --reload --quiet default &
 MONITOR="DVI-I-2-2" polybar --reload --quiet default &
 
-setxkbmap -layout us -variant intl
+psetxkbmap -model pc104 -layout us -variant altgr-intl
 
 pacmd set-default-sink "alsa_output.usb-DisplayLink_ThinkPad_Hybrid_USB-C_with_USB-A_Dock_10660913-02.analog-stereo"
