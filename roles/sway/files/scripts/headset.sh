24bac2571b3a2074859ba7a02bd7765b71e6c0f8
Joren Vrancken <jorenvrancken@gmail.com>
2023-08-17 18:14:56 +0200

    Add bluetooth module to waybar

--- roles/sway/files/scripts/headset.sh
+++ roles/sway/files/scripts/headset.sh
@@ -15,25 +15,33 @@ if bluetoothctl show | grep --quiet "Powered: no"; then
     fi
 fi
 
-if [[ $1 == "connect" ]]; then
+if [[ ${1} == "connect" ]]; then
     echo "Connecting to headset";
-    if bluetoothctl connect "$HEADSET_MAC_ADDRESS" > /dev/null; then
+    if bluetoothctl connect "${HEADSET_MAC_ADDRESS}" > /dev/null; then
         echo "Successfully connected";
     else
         echo "Connection failed";
         exit 1;
     fi
 
-elif [[ $1 == "disconnect" ]]; then
+elif [[ ${1} == "disconnect" ]]; then
     echo "Disconnecting headset";
-    if bluetoothctl disconnect "$HEADSET_MAC_ADDRESS" > /dev/null; then
+    if bluetoothctl disconnect "${HEADSET_MAC_ADDRESS}" > /dev/null; then
         echo "Successfully disconnected";
     else
         echo "Disconnecting failed";
         exit 1;
     fi
 
+elif [[ ${1} == "switch" ]]; then
+    echo "Switching headset state";
+    if bluetoothctl info "${HEADSET_MAC_ADDRESS}" | grep --quiet "Connected: no"; then
+        ${0} connect;
+    else
+        ${0} disconnect;
+    fi
+
 else
-    echo -e "Unknown option given: $1\nUsage: headset.sh connect|disconnect"
+    echo -e "Unknown option given: '${1}'\nUsage: headset.sh connect|disconnect|switch"
     exit 1;
 fi
